0f920d6a0b861db7f5f494bf6eff69e9388aa4ac
Øyvind Kallstad <kallstad@gmail.com>
2016-03-19 16:28:01 +0100

    First commit of new Speakers Gallery

--- js/custom.js
+++ js/custom.js
@@ -81,3 +81,33 @@ $(function () {
 $(function () {
   $('[data-toggle="popover"]').popover()
 })
+
+// Flow Gallery
+$('#speakers-gallery').flowGallery({
+            gridType: 'columns',
+            //columns: {320: 3, 480: 5, 720: 6, 960: 7, 1200: 8},
+            columns: 6,
+            //rowItemMinWidth: 150,
+            //justifyLastRow: false,
+            horizontalGutter: 10,
+            verticalGutter: 10,
+            items: {style: 'card'},
+            card: {
+                alwaysShowCaption: true,
+                descriptionInCaption: false
+            },
+            captionShowAnimation: 'pushUp',
+            enableCache: false,
+            enableDeepLinking: false,
+            loadItemChunks: false,
+            enableSocialShare: false,
+            showFileTypeIcons: false,
+            categoryFilters: ["United Kingdom","USA","Netherlands","Norway","Sweden","India","Germany","Serbia"],
+            showFileTypeFilters: false,
+            hideEmptyCategories: true,
+            showFullscreenButton: false,
+            sortBy: [],
+            fixMenuOnScroll: true,
+            menuFixOffset: 50,
+            configUrl: 'flowconfig.json'
+        });
