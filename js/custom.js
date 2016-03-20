efcddad478c7ee511c5d7ca0a8467e530c56e534
Øyvind Kallstad <kallstad@gmail.com>
2016-03-20 12:42:37 +0100

    Some small changes and optimizations + added missing speaker to gallery

--- js/custom.js
+++ js/custom.js
@@ -85,7 +85,7 @@ $(function () {
 // Flow Gallery
 $('#speakers-gallery').flowGallery({
             gridType: 'columns',
-            columns: {320: 2, 480: 4, 720: 4, 960: 6, 1200: 6},
+            columns: {320: 2, 480: 3, 720: 4, 960: 5, 1200: 6},
             //columns: 6,
             //rowItemMinWidth: 150,
             //justifyLastRow: false,
@@ -102,7 +102,7 @@ $('#speakers-gallery').flowGallery({
             loadItemChunks: false,
             enableSocialShare: false,
             showFileTypeIcons: false,
-            categoryFilters: ["United Kingdom","USA","Netherlands","Norway","Sweden","India","Germany","Serbia"],
+            categoryFilters: ["United Kingdom","USA","Netherlands","Norway","Sweden","India","Germany","Serbia","Poland"],
             showFileTypeFilters: false,
             hideEmptyCategories: true,
             showFullscreenButton: false,
